b025b2472fc9156e2fbb651dec5b0ac24c85cea9
Robin.Wang <3Jg$94sbQ@q1>
2025-06-17 17:14:34 +0800

    feat(project): add tiptap

---
+++ src/components/TipTapEditor.vue
@@ -0,0 +1,15 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script setup lang="ts">
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+const editor = useEditor({
+  content: "<p>I'm running Tiptap with Vue.js. 🎉</p>",
+  extensions: [StarterKit],
+})
+</script>
+
+<style scoped></style>
