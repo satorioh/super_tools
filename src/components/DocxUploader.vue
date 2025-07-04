44aec0af211cf4509fb9e7abbc4acf89f876d868
Robin.Wang <3Jg$94sbQ@q1>
2025-07-04 14:22:26 +0800

    feat(project): add DocxUploader

---
+++ src/components/DocxUploader.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-upload
+    ref="uploadRef"
+    v-model:file-list="fileList"
+    class="upload-demo"
+    action=""
+    :auto-upload="false"
+    accept=".docx"
+    @change="handleChange"
+  >
+    <template #trigger>
+      <el-button type="primary">上传.docx文件</el-button>
+    </template>
+  </el-upload>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { UploadInstance, UploadUserFile } from 'element-plus'
+
+defineOptions({
+  name: 'DocxUploader',
+})
+
+const uploadRef = ref<UploadInstance>()
+const fileList = ref<UploadUserFile[]>([])
+
+const handleChange = (file: UploadUserFile) => {
+  console.log('File changed:', file.raw)
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <template>
   <main>
-    <el-button type="primary">Go to Editor</el-button>
+    <DocxUploader />
     <Editor />
   </main>
 </template>
 
 <script setup lang="ts">
 import Editor from '@/components/TipTapEditor.vue'
+import DocxUploader from '@/components/DocxUploader.vue'
 </script>
