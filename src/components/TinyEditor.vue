067d91233a7a4a8fdbb3bd2611ad71ff267c3ed1
Robin.Wang <3Jg$94sbQ@q1>
2025-07-06 18:37:37 +0800

    feat(project): tiny editor add source code mode toggle

--- src/components/TinyEditor.vue
+++ src/components/TinyEditor.vue
@@ -27,11 +27,39 @@ const content = computed({
 
 const editorConfig = {
   selector: '#about',
-  plugins: 'code autoresize autosave link image',
+  plugins: 'code autoresize autosave link image preview',
   menubar: false,
   branding: false,
   toolbar:
-    'undo redo | fontsizeselect formatselect | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent blockquote | link image media',
+    'undo redo | fontsizeselect formatselect | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent blockquote | link image media | preview toggleMode',
+
+  setup: function (editor) {
+    let isSourceMode = false
+    let originalContent = ''
+
+    editor.ui.registry.addButton('toggleMode', {
+      text: '切换模式',
+      onAction: function () {
+        if (!isSourceMode) {
+          // 进入源码模式，保存原始 HTML 内容
+          originalContent = editor.getContent()
+          const encoded =
+            tinymce.util.Tools.resolve('tinymce.html.Entities').encodeAllRaw(originalContent)
+          editor.setContent(
+            `<pre id="tinymce-source-mode" style="white-space:pre-wrap;">${encoded}</pre>`,
+          )
+          isSourceMode = true
+        } else {
+          // 返回 WYSIWYG 模式
+          const pre = editor.getDoc().getElementById('tinymce-source-mode')
+          const rawHtml = pre ? pre.innerText : ''
+          const decoded = tinymce.util.Tools.resolve('tinymce.html.Entities').decode(rawHtml)
+          editor.setContent(decoded)
+          isSourceMode = false
+        }
+      },
+    })
+  },
 }
 </script>
 
