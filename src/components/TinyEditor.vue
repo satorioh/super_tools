91a4aa0df4633194e34e0b59a35a340d81c39ca8
Robin.Wang <3Jg$94sbQ@q1>
2025-07-06 17:51:37 +0800

    feat(project): add tiny editor

---
+++ src/components/TinyEditor.vue
@@ -0,0 +1,38 @@
+<template>
+  <Editor v-model="content" :init="editorConfig" tinymce-script-src="/tinymce/tinymce.min.js" />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import Editor from '@tinymce/tinymce-vue'
+
+defineOptions({
+  name: 'TinyEditor',
+})
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+})
+
+const emit = defineEmits(['update:modelValue'])
+const content = computed({
+  get: () => props.modelValue,
+  set: (value: string) => {
+    emit('update:modelValue', value)
+  },
+})
+
+const editorConfig = {
+  selector: '#about',
+  plugins: 'code autoresize autosave link image',
+  menubar: false,
+  branding: false,
+  toolbar:
+    'undo redo | fontsizeselect formatselect | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent blockquote | link image media',
+}
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,8 @@
     </div>
     <div class="content">
       <DocxConvert :file="docxFile?.raw" @converted="handleConverted" />
-      <Editor v-model="htmlContent" />
+      <!--      <TipTapEditor v-model="htmlContent" />-->
+      <TinyEditor v-model="htmlContent" />
     </div>
   </main>
 </template>
@@ -16,7 +17,8 @@ import { ref } from 'vue'
 import DocxUploader from '@/components/DocxUploader.vue'
 import DocxViewer from '@/components/DocxViewer.vue'
 import DocxConvert from '@/components/DocxConvert.vue'
-import Editor from '@/components/TipTapEditor.vue'
+import TipTapEditor from '@/components/TipTapEditor.vue'
+import TinyEditor from '@/components/TinyEditor.vue'
 import type { UploadUserFile } from 'element-plus'
 
 const docxFile = ref<UploadUserFile | null>(null)
